81259f449a0081152e6e7be9b40ff90ca20bfd36
Борис Распутин <bang-kirov@bk.ru>
2022-07-11 21:35:31 +0300

    Initial

---
+++ src/main/java/Birthday.java
@@ -0,0 +1,5 @@
+public class Birthday {
+    public int day;
+    public String month;
+    public int year;
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7-912-77-81-92";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        Birthday birthday = new Birthday();
+        birthday.day = 16;
+        birthday.month = "september";
+        birthday.year = 87;
+
+    }
+}
